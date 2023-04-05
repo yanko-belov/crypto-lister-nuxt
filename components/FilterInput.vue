741cb8ef7532f42b8d793869ae52e00a7ff2db3d
Yanko Belov <belov.yanko@gmail.com>
2023-04-05 15:20:51 +0300

    feat: Add filter to the list

---
+++ components/FilterInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <label class="sr-only mb-2">Filter by name or symbol</label>
+  <div class="relative">
+    <div
+      class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
+    >
+      <svg
+        aria-hidden="true"
+        class="h-5 w-5 text-gray-500 dark:text-gray-400"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        ></path>
+      </svg>
+    </div>
+    <input
+      type="search"
+      class="block w-full rounded-lg border border-gray-300 p-4 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+      placeholder="Filter by name or symbol"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+<script lang="ts" setup>
+withDefaults(defineProps<{ modelValue: string }>(), {
+  modelValue: "",
+});
+defineEmits(["update:modelValue"]);
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="container mx-auto px-4 py-6">
-    <h1 class="mb-6 text-3xl font-bold">List of Cryptocurrencies</h1>
+    <h1 class="mb-6 text-3xl font-bold">Home</h1>
+    <FilterInput v-model="filter" class="mt-3" />
     <div
-      class="grid grid-cols-1 gap-6 min-[530px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
+      class="mt-3 grid grid-cols-1 gap-6 min-[530px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
     >
       <NuxtLink
         v-for="currency in currencyList"
@@ -18,6 +19,14 @@
 <script setup>
 import { useCurrencyStore } from "~/store/currencyStore";
 
+const filter = ref("");
 const store = useCurrencyStore();
-const currencyList = computed(() => store.list);
+const currencyList = computed(() =>
+  store.list.filter((currency) => {
+    const name = currency.name.toLowerCase();
+    const symbol = currency.symbol.toLowerCase();
+    const filterValue = filter.value.toLowerCase();
+    return name.includes(filterValue) || symbol.includes(filterValue);
+  })
+);
 </script>
