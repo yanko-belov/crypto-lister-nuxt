4c42342e314ed19957e23e4d8fe4c966e2b3d38a
Yanko Belov <belov.yanko@gmail.com>
2023-04-05 18:08:45 +0300

    fix: Move data related images to own directory

--- components/CurrencyList.vue
+++ components/CurrencyList.vue
@@ -15,7 +15,7 @@
   </template>
   <template v-else>
     <div class="flex h-full flex-col items-center justify-center">
-      <img src="/img/no-data.svg" alt="No currencies found" class="w-96" />
+      <img src="/img/data/no-data.svg" alt="No currencies found" class="w-96" />
       <h1 class="mt-5 text-2xl font-bold">
         <slot name="no-data-header">No Currencies Found</slot>
       </h1>
