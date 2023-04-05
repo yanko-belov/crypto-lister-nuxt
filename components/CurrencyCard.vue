521bb7a4d6cad8c832fa369ad083249f37947e5d
Yanko Belov <belov.yanko@gmail.com>
2023-04-05 09:51:51 +0300

    feat: Move the currency to a CurrencyCard.vue

---
+++ components/CurrencyCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="group relative cursor-pointer">
+    <div
+      class="absolute -inset-0.5 rounded-lg bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 opacity-0 blur transition duration-500 group-hover:opacity-75"
+    ></div>
+    <div
+      class="flex-grow-1 relative flex h-full w-full flex-col rounded-md bg-white p-4 shadow-lg"
+    >
+      <div class="flex items-center">
+        <div class="grow">
+          <h2 class="text-xl font-bold">{{ currency.name }}</h2>
+          <h3 class="grow text-sm font-bold">Price: {{ priceFormatted }}</h3>
+        </div>
+        <img
+          class="h-10 w-10"
+          src="https://cryptologos.cc/logos/bitcoin-btc-logo.png"
+          alt="Bitcoin Logo"
+        />
+      </div>
+      <div class="mt-4 flex justify-end">
+        <div class="flex w-10 justify-center">
+          <img src="/img/chevron-right.svg" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{ currency: any }>();
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+const priceFormatted = computed(() =>
+  formatter.format(props.currency.quote["USD"].price)
+);
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -9,37 +9,12 @@
         :key="`currency-${currency.id}`"
         :to="`currency/${currency.symbol.toLowerCase()}`"
       >
-        <div class="group relative cursor-pointer">
-          <div
-            class="absolute -inset-0.5 rounded-lg bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 opacity-0 blur transition duration-500 group-hover:opacity-75"
-          ></div>
-          <div
-            class="flex-grow-1 relative flex h-full w-full flex-col rounded-md bg-white p-4 shadow-lg"
-          >
-            <div class="flex items-center">
-              <div class="grow">
-                <h2 class="text-xl font-bold">{{ currency.name }}</h2>
-                <h3 class="grow text-sm font-bold">
-                  Price: {{ formatter.format(currency.quote["USD"].price) }}
-                </h3>
-              </div>
-              <img
-                class="h-10 w-10"
-                src="https://cryptologos.cc/logos/bitcoin-btc-logo.png"
-                alt="Bitcoin Logo"
-              />
-            </div>
-            <div class="mt-4 flex justify-end">
-              <div class="flex w-10 justify-center">
-                <img src="/img/chevron-right.svg" alt="" />
-              </div>
-            </div>
-          </div>
-        </div>
+        <CurrencyCard :currency="currency" />
       </NuxtLink>
     </div>
   </div>
 </template>
+
 <script setup>
 const currencyList = [
   {
@@ -145,9 +120,4 @@ const currencyList = [
     },
   },
 ];
-
-const formatter = new Intl.NumberFormat("en-US", {
-  style: "currency",
-  currency: "USD",
-});
 </script>
