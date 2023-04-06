c6a2f54bddc6ea9cff80056658b1007f14f3b7ba
Yanko Belov <belov.yanko@gmail.com>
2023-04-06 11:36:56 +0300

    feat: Create currency composable to extract and share logic

---
+++ composables/currency.ts
@@ -0,0 +1,30 @@
+import { useCurrencyStore } from "~/store/currencyStore";
+export function useCurrency(symbolParam: string) {
+  const store = useCurrencyStore();
+
+  const symbol = computed(() => symbolParam.toLowerCase());
+
+  const isFavorite = computed(() => store.favorites.includes(symbol.value));
+  const toggleFavorite = () =>
+    isFavorite.value
+      ? store.removeFromFavorites(symbol.value)
+      : store.addToFavorites(symbol.value);
+
+  const currency = computed(() =>
+    store.list.find(
+      (currency) => currency.symbol.toLowerCase() === symbol.value
+    )
+  );
+
+  const hasValidCurrency = computed(() => currency.value !== undefined);
+
+  const formatPrice = store.getPriceFormatted;
+
+  return {
+    currency,
+    isFavorite,
+    formatPrice,
+    toggleFavorite,
+    hasValidCurrency,
+  };
+}
