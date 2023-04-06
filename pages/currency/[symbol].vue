1d1fcc5369645c30fb7f6c06b45e6b8bb4e16a27
Yanko Belov <belov.yanko@gmail.com>
2023-04-06 13:08:38 +0300

    feat: Handle currency not found in currency overview

--- pages/currency/[symbol].vue
+++ pages/currency/[symbol].vue
@@ -1,15 +1,32 @@
 <template>
   <CurrencyOverview v-if="hasValidCurrency" :currency="currency" />
+  <LoadingError v-else>
+    <template #title>
+      No currency is found with the symbol
+      <span class="font-bold text-red-500">{{ routesSymbol }}</span>
+    </template>
+    <template #cta>
+      <button
+        type="button"
+        class="mt-3 rounded-lg border border-red-700 px-5 py-2.5 text-center text-sm font-medium text-red-700 hover:bg-red-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-red-300"
+        @click="goHome"
+      >
+        Go back
+      </button>
+    </template>
+  </LoadingError>
 </template>
 
 <script lang="ts" setup>
 import { useCurrency } from "~/composables/currency";
 
-const route = useRoute();
+const router = useRouter();
 const routesSymbol = computed(
-  (): string => (route.params?.symbol as string) || ""
+  (): string => (router.currentRoute.value.params?.symbol as string) || ""
 );
 
 const { currency, hasValidCurrency } = useCurrency(routesSymbol.value);
+
+const goHome = () => router.push("/");
 </script>
 1
