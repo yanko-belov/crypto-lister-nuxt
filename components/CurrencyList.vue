11bca3af5463748d460f3478da7e68cabfe2d972
Yanko Belov <belov.yanko@gmail.com>
2023-04-06 23:18:02 +0300

    test: Add data-testid to CurrencyList.vue

--- components/CurrencyList.vue
+++ components/CurrencyList.vue
@@ -1,22 +1,37 @@
 <template>
-  <CurrencyListLoader v-if="props.isLoading" />
+  <CurrencyListLoader v-if="props.isLoading" data-testid="loader" />
   <template v-else-if="hasCurrencies">
-    <FilterInput v-model="filter" />
+    <FilterInput v-model="filter" data-testid="currency-filter" />
     <div
       class="mt-4 grid grid-cols-1 gap-4 min-[530px]:grid-cols-2 md:mt-6 md:grid-cols-3 md:gap-6 lg:grid-cols-4 xl:grid-cols-5"
+      data-testid="card-container"
     >
       <NuxtLink
         v-for="currency in currencyListFiltered"
         :key="`currency-${currency.id}`"
         :to="`currency/${currency.symbol.toLowerCase()}`"
+        data-testid="card-link"
       >
-        <CurrencyCard :symbol="currency.symbol.toLowerCase()" class="h-full" />
+        <CurrencyCard
+          data-testid="card"
+          :symbol="currency.symbol.toLowerCase()"
+          class="h-full"
+        />
       </NuxtLink>
     </div>
   </template>
-  <div v-else class="flex h-full flex-col items-center justify-center">
-    <img src="/img/data/no-data.svg" alt="No currencies found" class="w-96" />
-    <h1 class="mt-5 text-2xl font-bold">
+  <div
+    v-else
+    data-testid="missing-data"
+    class="flex h-full flex-col items-center justify-center"
+  >
+    <img
+      data-testid="missing-data-image"
+      src="/img/data/no-data.svg"
+      alt="No currencies found"
+      class="w-96"
+    />
+    <h1 data-testid="missing-data-title" class="mt-5 text-2xl font-bold">
       <slot name="no-data-header">No Currencies Found</slot>
     </h1>
     <slot name="cta"></slot>
