e67faba3ea515f27343ac510ffd9fb154b07bc08
Yanko Belov <belov.yanko@gmail.com>
2023-04-06 21:34:58 +0300

    test: Prepare CurrencyCard.vue for testing by adding data-testid to elements

--- components/CurrencyCard.vue
+++ components/CurrencyCard.vue
@@ -8,12 +8,13 @@
     >
       <div class="flex gap-1">
         <div class="grow text-zinc-700">
-          <h2 class="text-xl">{{ currency.name }}</h2>
-          <h3 class="mt-2 text-sm font-bold">
+          <h2 data-testid="title" class="text-xl">{{ currency.name }}</h2>
+          <h3 data-testid="formatted-price" class="mt-2 text-sm font-bold">
             {{ formattedPrice }}
           </h3>
         </div>
         <img
+          data-testid="logo"
           class="h-10 w-10"
           :src="`/img/svg-crypto-logos/${symbol}.svg`"
           alt="Logo"
@@ -21,12 +22,14 @@
       </div>
       <div class="mt-4 flex items-center justify-between">
         <img
+          data-testid="toggle-favorite-button"
           class="h-8 w-8"
           :src="`/img/icons/favorite-${isFavorite ? '' : 'in'}active.svg`"
           alt="Favorite toggle"
           @click.stop.prevent="toggleFavorite"
         />
         <img
+          data-testid="currency-details-button"
           class="h-6 w-6"
           src="/img/icons/chevron-right.svg"
           alt="View Details"
