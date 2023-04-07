87f542506eb60a898b3c4509d762a81d1742904d
Yanko Belov <belov.yanko@gmail.com>
2023-04-07 10:45:47 +0300

    test: Fix tests and use deep mount

--- tests/components/CurrencyList.test.ts
+++ tests/components/CurrencyList.test.ts
@@ -1,14 +1,18 @@
-import { RouterLinkStub, shallowMount } from "@vue/test-utils";
+import { RouterLinkStub, mount } from "@vue/test-utils";
 import CurrencyList from "~/components/CurrencyList.vue";
 import CurrencyCard from "~/components/CurrencyCard.vue";
 import CurrencyListLoader from "~/components/CurrencyListLoader.vue";
 import FilterInput from "~/components/FilterInput.vue";
 
-import { currencyList as currencyListMocked } from "~/tests/_data";
+import {
+  currencyList,
+  currencyList as currencyListMocked,
+} from "~/tests/_data";
 import { createTestingPinia } from "@pinia/testing";
 import { setActivePinia } from "pinia";
 import { nextTick } from "vue";
 import type { ICurrency } from "~/types";
+import { useCurrencyStore } from "~/store/currency";
 
 const mountCurrencyList = ({
   currencyList,
@@ -17,7 +21,7 @@ const mountCurrencyList = ({
   currencyList: ICurrency[];
   isLoading: boolean;
 }) => {
-  return shallowMount(CurrencyList, {
+  return mount(CurrencyList, {
     props: {
       currencyList,
       isLoading,
@@ -34,8 +38,12 @@ const mountCurrencyList = ({
 };
 
 describe("Currency List Test", () => {
+  let store = null;
   beforeEach(() => {
     setActivePinia(createTestingPinia({ stubActions: false }));
+
+    store = useCurrencyStore();
+    store.list = currencyList;
   });
 
   test("component exist", async () => {
@@ -61,7 +69,9 @@ describe("Currency List Test", () => {
       currencyList: currencyListMocked,
       isLoading: true,
     });
-    expect(wrapper.find("[data-testid='loader']").exists()).toBeTruthy();
+    expect(
+      wrapper.find("[data-testid='currency-list-loader']").exists()
+    ).toBeTruthy();
   });
 
   test("test listing", async () => {
@@ -69,14 +79,11 @@ describe("Currency List Test", () => {
       currencyList: currencyListMocked,
       isLoading: false,
     });
-    expect(
-      wrapper.find("[data-testid='currency-filter']").exists()
-    ).toBeTruthy();
+    expect(wrapper.find("[data-testid='filter-input']").exists()).toBeTruthy();
     expect(
       wrapper.find("[data-testid='card-container']").exists()
     ).toBeTruthy();
-    expect(wrapper.find("[data-testid='card-link']").exists()).toBeTruthy();
-    expect(wrapper.find("[data-testid='card']").exists()).toBeTruthy();
+    expect(wrapper.find("[data-testid='currency-card']").exists()).toBeTruthy();
   });
 
   test("test filtering", async () => {
@@ -88,11 +95,11 @@ describe("Currency List Test", () => {
     wrapper.vm.filter = "btc";
     await nextTick();
     expect(
-      wrapper.findAll("[data-testid='card-link']").length
+      wrapper.findAll("[data-testid='currency-card']").length
     ).toBeGreaterThanOrEqual(1);
 
     wrapper.vm.filter = "btc123123123123";
     await nextTick();
-    expect(wrapper.findAll("[data-testid='card-link']").length).toBe(0);
+    expect(wrapper.findAll("[data-testid='currency-card']").length).toBe(0);
   });
 });
